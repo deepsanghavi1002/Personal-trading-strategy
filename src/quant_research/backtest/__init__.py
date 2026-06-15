2a6cb8155dad77b73d8c80923ca4cc6c05a63611
Deep Sanghavi <deepsanghavi1002@gmail.com>
2026-06-15 09:54:57 -0400

    Add backtest package exports

---
+++ src/quant_research/backtest/__init__.py
@@ -0,0 +1,4 @@
+from quant_research.backtest.engine import BacktestResult, run_backtest
+from quant_research.backtest.metrics import performance_summary
+
+__all__ = ["BacktestResult", "run_backtest", "performance_summary"]
